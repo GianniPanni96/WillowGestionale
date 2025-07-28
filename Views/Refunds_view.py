08b9d1b41e42ef79b27d24c4eec83099c0bcb6a6
Gianluca PANZA <gianluca.panza@alten.it>
2025-07-28 22:34:41 +0200

    wip refunds detail view

--- Views/Refunds_view.py
+++ Views/Refunds_view.py
@@ -30,11 +30,29 @@ class RefundsView(ctk.CTkFrame):
         self.refund = {}
         self.cards_warnings = {}
 
+        # Container principale
+        self.main_container = ctk.CTkFrame(self, fg_color="#2b2b2b")
+        self.detail_container = ctk.CTkFrame(self, fg_color="#2b2b2b")
+
+        # Vista dettaglio
+        self.refund_detail_view = RefundDetailView(
+            parent=self,
+            back_callback=self.show_main_view,
+            client_controller=self.client_controller,
+            account_controller=account_controller,
+            refund_controller=refunds_controller,
+            db_model=db_model,
+            analyzer=self.analyzer,
+            event_bus = self.event_bus,
+        )
+
         self.create_refunds_tab()
+        self.show_main_view()
+
 
     def create_refunds_tab(self):
 
-        self.search_bar_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.search_bar_frame = ctk.CTkFrame(self.main_container, fg_color="transparent")
         self.search_bar_frame.pack(pady=(5, 10), fill="x", anchor="s")
         self.search_bar = ctk.CTkEntry(self.search_bar_frame)
         self.search_bar.pack(padx=(5, 35), anchor="s", side="right")
@@ -69,7 +87,7 @@ class RefundsView(ctk.CTkFrame):
             # salvo i dati che potrebbero avere bisogno di configure successivamente
             self.amount_aggregate_labels[f"{key}"] = amount
 
-        self.refunds_table_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.refunds_table_frame = ctk.CTkFrame(self.main_container, fg_color="transparent")
         self.refunds_table_frame.pack(pady=(20, 0), padx=(10, 15), fill="x", anchor="n")
 
         self.table_headers = ["NOME", "CLIENTE", "TOTALE", "DATA", "CONTO\nCORRENTE"]
@@ -89,10 +107,10 @@ class RefundsView(ctk.CTkFrame):
             label.pack(fill="both", expand=True, padx=5, pady=15)
 
         # Creazione del frame delle cards
-        self.refunds_cards_frame = ctk.CTkScrollableFrame(self, fg_color="transparent")
+        self.refunds_cards_frame = ctk.CTkScrollableFrame(self.main_container, fg_color="transparent")
         self.refunds_cards_frame.pack(padx=0, pady=10, fill="both", expand=True)
 
-        self.add_refund_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.add_refund_frame = ctk.CTkFrame(self.main_container, fg_color="transparent")
         self.add_refund_frame.pack(padx=0, pady=(5, 20), fill="x")
 
         self.save_button = ctk.CTkButton(self.add_refund_frame, text="Aggiungi un rimborso",
@@ -132,6 +150,17 @@ class RefundsView(ctk.CTkFrame):
         # warnings launch
         for card in self.refund_card_list.values():
             ViewUtils.toggle_warning_on_card(card, self.cards_warnings)
+
+    def show_main_view(self):
+        """Torna alla vista principale"""
+        self.refund_detail_view.pack_forget()
+        self.main_container.pack(fill='both', expand=True)
+
+    def open_refund_detail_tab(self, refund_id):
+        """Mostra la vista dettaglio del rimborso"""
+        self.main_container.pack_forget()
+        self.refund_detail_view.pack(fill='both', expand=True)
+        self.refund_detail_view.create_detail_tab(refund_id)  # Ricrea i contenuti ogni volta
 
     def filter_cards(self, event):
         """Filtra le card in base al testo della barra di ricerca e al tipo di filtro scelto."""
@@ -371,3 +400,110 @@ class RefundsView(ctk.CTkFrame):
     def clear_class_variable(self):
         self.refund_widgets.clear()
         self.refund_widgets.clear()
+
+
+
+
+class RefundDetailView(ctk.CTkFrame):
+    def __init__(self, parent, back_callback, client_controller, account_controller, refund_controller, db_model, analyzer, event_bus):
+        super().__init__(parent)
+        self.refund_controller = refund_controller
+        self.db_model = db_model
+        self.back_callback = back_callback
+        self.client_controller = client_controller
+        self.account_controller = account_controller
+        self.event_bus = event_bus
+        self.current_refund_id = None
+        self.analyzer = analyzer
+
+        self.configure(fg_color="transparent")
+
+        # Widgets persistenti (vanno creati una volta sola)
+        self.head_frame = ctk.CTkFrame(self, fg_color="#2b2b2b")
+        self.back_button = ctk.CTkButton(
+            self.head_frame,
+            text="Elenco Clienti",
+            command=self._cleanup_and_go_back
+        )
+        self.title_label = ctk.CTkLabel(self.head_frame, font=("Arial", 22, "bold"))
+
+        self.user_info_widgets: dict[str, ctk.CTkEntry | ctk.CTkOptionMenu] = {}
+
+        self.nome_cliente_string = "CLIENTE ASSOCIATo"
+        self.nome_conto_string = "CONTO"
+
+
+        # Container per i contenuti dinamici
+        self.content_frame = ctk.CTkScrollableFrame(self)
+
+        self.switch_modify = ctk.CTkSwitch(self.head_frame, text="Abilita la modifica", command=lambda: self.toggle_edit(self.content_frame))
+
+        # Layout iniziale
+        self._setup_base_layout()
+
+
+    def _setup_base_layout(self):
+        """Inizializza la struttura base del layout"""
+        self.head_frame.pack(fill="x", pady=5, padx=5)
+        self.back_button.pack(anchor="w", side="left", pady=10, padx=10)
+        self.title_label.pack(anchor="c", side="left", fill="x", expand=True, pady=10)
+        self.switch_modify.pack(anchor="e", side="left", pady=10, padx=10)
+        self.content_frame.pack(fill="both", expand=True, pady=20, padx=20)
+
+    def create_detail_tab(self, refund_id):
+        """Ricrea la vista dettaglio per un rimborso specifico"""
+        self.current_refund_id = refund_id
+
+        # 1. Pulizia dei widget precedenti
+        self._clear_content()
+
+        # 2. Caricamento dati
+        self.refund = self.refund_controller.retrieve_refund_map_by_id(refund_id)
+
+        # 3. Aggiornamento elementi persistenti
+        self.title_label.configure(
+            text=f"{self.refund[DBRefundsColumns.REFUND_NAME.value]}")
+
+        # 4. Creazione contenuti dinamici
+        self._create_refund_info_section(self.refund)
+        self.toggle_edit(self.content_frame)
+
+        self.wrapper_frame = ctk.CTkFrame(self.content_frame, fg_color="#333333")
+        self.wrapper_frame.pack(padx=25, pady=(90, 0), fill="both", expand=True)
+        #self.wrapper_frame2 = ctk.CTkFrame(self.content_frame, fg_color="#333333")
+        #self.wrapper_frame2.pack(padx=25, pady=(90, 90), fill="both", expand=True)
+
+
+    def toggle_edit(self, parent):
+        """
+        Abilita o disabilita la modifica dei widget nella finestra di modifica utente.
+        """
+        # Determina lo stato (abilitato/disabilitato) in base al valore dello switch
+        state = ctk.NORMAL if self.switch_modify.get() else ctk.DISABLED
+
+        # Cambia anche lo stato del pulsante Salva
+        self.save_info_btn.configure(state=state)
+        self.delete_btn.configure(state=state)
+
+        for w in parent.winfo_children():
+            # se è un Entry
+            if isinstance(w, (ctk.CTkEntry, ctk.CTkTextbox)):
+                w.configure(state=state, text_color="#636363" if state == ctk.DISABLED else "#c2c2c2")
+            # se è un OptionMenu
+            elif isinstance(w, ctk.CTkOptionMenu):
+                w.configure(state=state)
+            # se è un Frame/container, scendi ricorsivamente
+            elif isinstance(w, (ctk.CTkFrame, ctk.CTkScrollableFrame, ctk.CTkToplevel)):
+                self.toggle_edit(w)
+
+    def _clear_content(self):
+        """Distrugge tutti i widget dinamici"""
+        for widget in self.content_frame.winfo_children():
+            widget.destroy()
+
+    def _cleanup_and_go_back(self):
+        """Pulizia completa prima di tornare indietro"""
+        self._clear_content()
+        self.pack_forget()
+        self.back_callback()
+
